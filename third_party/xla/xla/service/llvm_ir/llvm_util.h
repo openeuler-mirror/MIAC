f0d40905634c39e8577ae07ad33903187f2be2b2
Jinyun (Joey) Ye <jinunye@huawei.com>
2025-12-18 23:55:14 +0800

    [Huawei] Propagation of dynamic dimension multiplier
    Author: cuixiaofeng 00444796 <cuixiaofeng1@huawei.com>

--- third_party/xla/xla/service/llvm_ir/llvm_util.h
+++ third_party/xla/xla/service/llvm_ir/llvm_util.h
@@ -342,7 +342,7 @@ void EmitEarlyReturn(llvm::Value* condition, llvm::IRBuilderBase* b,
 
 llvm::Value* GetBatchDimByName(llvm::IRBuilderBase* b);
 
-llvm::Value* GetBatchDimByPtr(llvm::IRBuilderBase* b);
+llvm::Value* GetBatchDimByPtr(llvm::IRBuilderBase* b, int64_t multiplier = 1);
 
 }  // namespace llvm_ir
 }  // namespace xla
