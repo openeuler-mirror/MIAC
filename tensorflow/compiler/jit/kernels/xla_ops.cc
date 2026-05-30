db5e475c3511f71ad7f99f8d7989c65c06c9381e
fengxiao-cui <cuixiaofeng1@huawei.com>
2026-05-30 18:05:18 +0800

    Change padding batch policy
    Previously, we used a padding policy of powers of 2, which would
    result in the loss of the multiple relationship. This commit changes
    the padding policy to a multiple padding with k value.
    
    All padding shapes are recorded for each function, if there exists a
    upper_bound for k*dim, select the upper_bound shape as padding shape

--- tensorflow/compiler/jit/kernels/xla_ops.cc
+++ tensorflow/compiler/jit/kernels/xla_ops.cc
@@ -614,7 +614,7 @@ absl::Status CompileToLocalExecutable(
             record_dynamic_dim_value(shp.dim_size(idx), xla::DExpr::Var(1));
             if (!filled_batch && xla_batch_matcher) {
               filled_batch =
-                  xla_batch_matcher->get_xla_compile_batch(shp.dim_size(idx));
+                  xla_batch_matcher->get_xla_compile_batch(function.name(), shp.dim_size(idx));
             }
 
             std::vector<xla::DExpr> dyn_exprs;
@@ -654,8 +654,10 @@ absl::Status CompileToLocalExecutable(
                           << shp.dim_size(idx) << " to " << var_value;
                 }
                 record_dynamic_dim_value(var_value, e);
+                VLOG(1) << "Filled batch for function " << function.name()
+                          << " with value " << var_value;
                 filled_batch =
-                    xla_batch_matcher->get_xla_compile_batch(var_value);
+                    xla_batch_matcher->get_xla_compile_batch(function.name(), var_value);
                 break;
               }
             }
